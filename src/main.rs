d03920080533e7b9fbf3229067f19a050f1681fa
Michael Szymczak <contact@michaelszymczak.com>
2021-07-31 19:44:23 +0100

    write a simple io code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::cmp::Ordering::{Equal, Greater};
+use std::io;
+
+use rand;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let lower_bound = 1;
+    let upper_bound = 20;
+    let secret_number: u32 = rand::thread_rng().gen_range(lower_bound..(upper_bound + 1));
+
+    loop {
+        let mut guess = String::new();
+        println!(
+            "Guess a number that is between {} and {}",
+            lower_bound, upper_bound
+        );
+        io::stdin().read_line(&mut guess).expect("error");
+
+        let x: u32 = match guess.trim().parse() {
+            Ok(number) => number,
+            Err(_) => 0,
+        };
+
+        let ordering = x.cmp(&secret_number);
+        if ordering == Equal {
+            println!("correct");
+            break;
+        } else if ordering == Greater {
+            println!("too high");
+        } else {
+            println!("too low");
+        }
+    }
 }
