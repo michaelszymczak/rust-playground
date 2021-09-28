37d5625eb95e00df484eeff04a6ff9150cbc0b0c
Michael Szymczak <contact@michaelszymczak.com>
2021-09-28 22:10:09 +0100

    sandbox

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,25 @@ use std::net::{TcpListener, TcpStream};
 // }
 // New prject vvvvvvvv
 fn main() {
-    let port = "7878";
-    println!("listening on {}", port);
-    let listener = TcpListener::bind(format!("{}:{}", "127.0.0.1", port)).unwrap();
-    for tcp_accept_result in listener.incoming() {
-        let stream = tcp_accept_result.unwrap();
-        println!(
-            "Connection {} <-> {} established",
-            stream.local_addr().unwrap(),
-            stream.peer_addr().unwrap()
-        );
+    if 2 > 1 {
+        sanbox();
+    } else {
+        let port = "7878";
+        println!("listening on {}", port);
+        let listener = TcpListener::bind(format!("{}:{}", "127.0.0.1", port)).unwrap();
+        for tcp_accept_result in listener.incoming() {
+            let stream = tcp_accept_result.unwrap();
+            println!(
+                "Connection {} <-> {} established",
+                stream.local_addr().unwrap(),
+                stream.peer_addr().unwrap()
+            );
 
-        handle_connection(stream);
-        // println!("Done handling {}", stream.peer_addr().unwrap());
+            handle_connection(stream);
+            // println!("Done handling {}", stream.peer_addr().unwrap());
+        }
+        println!("done")
     }
-    println!("done")
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -31,3 +35,7 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
+
+/////////////////////////////// sandbox //////////////////////////////////////////////
+
+fn sanbox() {}
