48abe1e52fff4373dc4d8380bf6236fbd18518cf
Michael Szymczak <contact@michaelszymczak.com>
2021-10-03 14:03:14 +0100

    conditional http responses

--- src/main.rs
+++ src/main.rs
@@ -29,21 +29,38 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     println!("Handling {}", stream.peer_addr().unwrap());
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer));
+    let size = stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..size]));
 
-    let response = response();
+    let response = response(&buffer);
+    match response {
+        Some(response_content) => {
+            stream.write(response_content.as_bytes()).unwrap();
+            stream.flush().unwrap();
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-
-    println!("Response: {}", response);
+            println!("Response: {}", response_content);
+        }
+        None => println!("No response"),
+    }
 }
 
-fn response() -> String {
-    let body = fs::read_to_string("hello.html").unwrap();
-    let headers = format!("Content-Length:{}", body.len());
-    let response_result = "HTTP/1.1 200 OK";
-    let response = format!("{}\r\n{}\r\n\r\n{}", response_result, headers, body);
-    response
+fn response(request_buffer: &[u8]) -> Option<String> {
+    return match response_body(request_buffer) {
+        Some(body) => {
+            let headers = format!("Content-Length:{}", body.len());
+            let response_result = "HTTP/1.1 200 OK";
+            let response = format!("{}\r\n{}\r\n\r\n{}", response_result, headers, body);
+            Some(response)
+        }
+        None => None,
+    };
+}
+
+fn response_body(request_buffer: &[u8]) -> Option<String> {
+    if request_buffer.starts_with(b"GET / HTTP") {
+        return Some(fs::read_to_string("hello.html").unwrap());
+    } else if request_buffer.starts_with(b"GET /favicon.ico HTTP") {
+        return Some(String::from(""));
+    }
+    None
 }
