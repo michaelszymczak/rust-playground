63c8060573fd70b5878dd4c5b8e1fed17752b970
Michael Szymczak <contact@michaelszymczak.com>
2021-10-03 19:34:17 +0100

    extract a lib from the main

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,5 @@
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
-use std::time::Duration;
-use std::{fs, thread};
-
-struct ThreadPool;
-
-impl ThreadPool {
-    pub fn new(_size: usize) -> ThreadPool {
-        ThreadPool
-    }
-    pub fn execute<F>(&self, f: F)
-    where
-        F: FnOnce() + Send + 'static,
-    {
-        thread::spawn(f);
-    }
-}
+use hello::ThreadPool;
+use std::net::TcpListener;
 
 // mod guessgame;
 //
@@ -37,46 +21,8 @@ fn main() {
                 stream.peer_addr().unwrap()
             );
 
-            handle_connection(stream);
+            hello::handle_connection(stream);
         });
     }
     println!("done")
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    println!("Handling {}", stream.peer_addr().unwrap());
-    let mut buffer = [0; 1024];
-    let size = stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..size]));
-    stream.write(response(&buffer).as_bytes()).unwrap();
-    stream.flush().unwrap();
-}
-
-fn response(request_buffer: &[u8]) -> String {
-    return match response_body(request_buffer) {
-        Some(body) => {
-            let headers = format!("Content-Length:{}", body.len());
-            let response_result = "HTTP/1.1 200 OK";
-            format!("{}\r\n{}\r\n\r\n{}", response_result, headers, body)
-        }
-        None => {
-            let body = fs::read_to_string("404.html").unwrap();
-            let headers = format!("Content-Length:{}", body.len());
-            let response_result = "HTTP/1.1 404 NOT FOUND";
-            format!("{}\r\n{}\r\n\r\n{}", response_result, headers, body)
-        }
-    };
-}
-
-fn response_body(request_buffer: &[u8]) -> Option<String> {
-    if request_buffer.starts_with(b"GET / HTTP") {
-        Some(fs::read_to_string("hello.html").unwrap())
-    } else if request_buffer.starts_with(b"GET /sleep HTTP") {
-        thread::sleep(Duration::from_secs(5));
-        Some(fs::read_to_string("hello.html").unwrap())
-    } else if request_buffer.starts_with(b"GET /favicon.ico HTTP") {
-        Some(String::from(""))
-    } else {
-        None
-    }
 }
