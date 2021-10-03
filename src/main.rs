7750d97fbf03e76df99da5ec9e3fda027b2c8573
Michael Szymczak <contact@michaelszymczak.com>
2021-10-03 19:16:49 +0100

    adda naive multi threaded request handler

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,15 @@ fn main() {
     let listener = TcpListener::bind(format!("{}:{}", "127.0.0.1", port)).unwrap();
     for tcp_accept_result in listener.incoming() {
         let stream = tcp_accept_result.unwrap();
-        println!(
-            "Connection {} <-> {} established",
-            stream.local_addr().unwrap(),
-            stream.peer_addr().unwrap()
-        );
+        thread::spawn(|| {
+            println!(
+                "Connection {} <-> {} established",
+                stream.local_addr().unwrap(),
+                stream.peer_addr().unwrap()
+            );
 
-        handle_connection(stream);
-        // println!("Done handling {}", stream.peer_addr().unwrap());
+            handle_connection(stream);
+        });
     }
     println!("done")
 }
