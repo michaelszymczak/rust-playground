167b6a1ecef6fd71a5ec7b0ead13a52977bee344
Michael Szymczak <contact@michaelszymczak.com>
2021-12-31 01:52:05 +0000

    play with some simple calculations and collection manipulations

--- misc001/src/lib.rs
+++ misc001/src/lib.rs
@@ -1,6 +1,22 @@
-pub enum Foo {
-    Bar,
-    Baz,
+use std::collections::HashMap;
+
+pub fn distance_distribution(range: i32) -> Vec<(i32, usize)> {
+    let mut distance_frequency: HashMap<i32, usize> = HashMap::new();
+    for a in 0..range {
+        for b in 0..range {
+            *distance_frequency
+                .entry(((a - b) as i32).abs())
+                .or_insert(0) += 1;
+        }
+    }
+
+    let mut result = distance_frequency
+        .into_iter()
+        .map(|(diff, freq)| (diff, freq))
+        .collect::<Vec<(i32, usize)>>();
+    result.sort_by(|(_, freq_a), (_, freq_b)| freq_b.cmp(freq_a));
+
+    result
 }
 
 pub fn add_one(value: i32) -> i32 {
--- misc001/tests/some_test.rs
+++ misc001/tests/some_test.rs
@@ -1,6 +1,18 @@
 use misc001;
+use misc001::distance_distribution;
 
 #[test]
 fn it_adds_one() {
     assert_eq!(4, misc001::add_one(3));
+}
+
+#[test]
+fn it_calculates_distance_distribution() {
+    //let mut diffs = vec![];
+    let distance_frequency = distance_distribution(10);
+
+    assert_eq!(10, distance_frequency.len());
+    assert_eq!(Some((1, 18)), distance_frequency.get(0).copied());
+    assert_eq!(Some((9, 2)), distance_frequency.get(9).copied());
+    println!("{:?}", distance_frequency);
 }
